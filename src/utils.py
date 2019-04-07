5393db57d05be9614c934b1912f61f7f7dfd81b6
buptzh111 <49114796+buptzh111@users.noreply.github.com>
2019-04-07 20:29:26 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -51,13 +51,17 @@ def get_association_matrix(G):
 
     G1 = np.empty(G.shape, dtype=np.float32)
     G1[0][0] = G[0][0]
+    Gc = np.empty(G.shape, dtype=np.float32)
+    Gc[0][0] = G[0][0]
     for i in xrange(m):
 
         for j in xrange(n):
             if i == 0:
                 a = 0
+                a1 = 0
             else:
                 a = G1[i - 1][j]
+                a1 = Gc[i - 1][j]
             if j == 0:
                 b = 0
             else:
@@ -67,17 +71,7 @@ def get_association_matrix(G):
             else:
                 c = 0
             G1[i][j] = G[i][j] + a + b - c
-
-    Gc = np.empty(G.shape, dtype=np.float32)
-    Gc[0][0] = G[0][0]
-
-    for i in xrange(m):
-        for j in xrange(n):
-            if i == 0:
-                a = 0
-            else:
-                a = Gc[i - 1][j]
-            Gc[i][j] = G[i][j] + a
+            Gc[i][j] = G[i][j] + a1
     return G1, Gc
 
 
@@ -85,7 +79,6 @@ def Minimum_Rectangle(G, r):
 
     m = G.shape[0]
     n = G.shape[1]
-    #print m, n
     G1, Gc = get_association_matrix(G)
     i = 0
     j = 0
@@ -98,6 +91,9 @@ def Minimum_Rectangle(G, r):
     for i1 in xrange(1, m):
         for i2 in xrange(i1, m):
             a = (Gc[i2, :] - Gc[i1 - 1, :])
+            s = G1[i2, n - 1] - G1[i1 - 1, n - 1]
+            if s < T:
+                continue
             j1, j2, S0 = shortestSubarray(a, T)
             if j1 > 0 and j2 > 0:
                 w0 = j2 - j1 + 1
@@ -138,3 +134,60 @@ def shortestSubarray(a, T):
                 S0 = S0 - a[st]
                 st += 1
     return j1, j2, Smin
+
+def IOU(Bi, Bj):
+    x11 = float(Bi[0])
+    x12 = float(Bi[1])
+    y11 = float(Bi[2])
+    y12 = float(Bi[3])
+
+    x21 = float(Bj[0])
+    x22 = float(Bj[1])
+    y21 = float(Bj[2])
+    y22 = float(Bj[3])
+
+    # print [x11,x12,y11,y12], [x21,x22,y21,y22]
+    endx = max(x12, x22)
+    startx = min(x11, x21)
+    endy = max(y12, y22)
+    starty = min(y11, y21)
+    width1 = x12 - x11
+    width2 = x22 - x21
+
+    height1 = y12 - y11
+    height2 = y22 - y21
+
+    s1 = width1 * height1
+    s2 = width2 * height2
+    width = width1 + width2 - (endx - startx)
+    height = height1 + height2 - (endy - starty)
+
+    if width <= 0 or height <= 0:
+        return 0
+    else:
+        return width * height / float(s1 + s2 - width * height)
+
+def compute_l(r, l):
+
+    if not isinstance(r, np.ndarray):
+        assert len(r) == 4, 'Argument 1 error.Check your data type.'
+        r = np.array(r)
+    if not isinstance(l, np.ndarray):
+        assert len(l) == 4, 'Argument 2 error.Check your data type.'
+        l = np.array(l)
+    assert r.shape == l.shape
+    lossx = np.linalg.norm(r - l)
+    lossx = lossx / 4.0
+    return lossx
+
+def create_default_box(thr, perm):
+
+    td = (1 - thr) / 2.0
+    x1 = td
+    x2 = (1 - td)
+    y1 = td
+    y2 = (1 - td)
+    if perm == 'draw':
+        return y1, x1, y2, x2
+    elif perm == 'normal':
+        return x1, x2, y1, y2
